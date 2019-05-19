765cf0f14cacfd797f6d6d5d1b10d771c86fcd25
MyPC <MyPC@DESKTOP-05LKFP1>
2019-05-19 22:15:57 +0200

    Add project files.

---
+++ FoxLearnDotNetMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoxLearnDotNetMVC.Startup))]
+namespace FoxLearnDotNetMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
