653799c7a93035cb7b7011a77385d23c4b4ef010
rndev80 <rndev80@gmail.com>
2019-05-20 00:53:36 +0200

    Add Full Name and Gender to Model and Updated view

---
+++ FoxLearnDotNetMVC/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace FoxLearnDotNetMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FoxLearnDotNetMVC.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "FoxLearnDotNetMVC.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(FoxLearnDotNetMVC.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- FoxLearnDotNetMVC/Models/AccountViewModels.cs
+++ FoxLearnDotNetMVC/Models/AccountViewModels.cs
@@ -77,6 +77,9 @@ namespace FoxLearnDotNetMVC.Models
         [Display(Name = "User Name"), Required]
         public string UserName { get; set; }
 
+        [Display(Name = "Full Name"), Required]
+        public string FullName { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
@@ -87,6 +90,8 @@ namespace FoxLearnDotNetMVC.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public bool Gender { get; set; }
     }
 
     public class ResetPasswordViewModel
