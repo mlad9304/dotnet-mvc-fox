f4ed2ecf4f083898491bd2e3b8c3a5cc28dca02b
rndev80 <rndev80@gmail.com>
2019-05-20 16:31:32 +0200

    Implemented custom password hasher

--- FoxLearnDotNetMVC/App_Start/IdentityConfig.cs
+++ FoxLearnDotNetMVC/App_Start/IdentityConfig.cs
@@ -38,6 +38,7 @@ namespace FoxLearnDotNetMVC
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
             : base(store)
         {
+            PasswordHasher = new CustomPasswordHasher();
         }
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
---
+++ FoxLearnDotNetMVC/Encrypt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace FoxLearnDotNetMVC
+{
+    public class Encrypt
+    {
+        public static string GetMD5Hash(string input)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] b = Encoding.UTF8.GetBytes(input);
+            b = md5.ComputeHash(b);
+            StringBuilder sb = new StringBuilder();
+            foreach (var x in b)
+            {
+                sb.Append(x.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
